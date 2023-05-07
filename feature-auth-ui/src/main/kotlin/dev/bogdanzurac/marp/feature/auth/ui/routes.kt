80db8fc9dae9a6a8cf0c84d477032ea554823ed3
Bogdan Zurac <bogdan.zurac@gmail.com>
2023-05-07 16:02:04 +0300

    Migrated feature-auth-ui from marp-app-client-android repo

---
+++ feature-auth-ui/src/main/kotlin/dev/bogdanzurac/marp/feature/auth/ui/routes.kt
@@ -0,0 +1,9 @@
+package dev.bogdanzurac.marp.feature.auth.ui
+
+import dev.bogdanzurac.marp.core.navigation.AppRoute.SimpleRoute
+
+object Auth : SimpleRoute("auth")
+
+object Login : SimpleRoute("auth/login")
+
+object Signup : SimpleRoute("auth/signup")
---
+++ feature-auth-ui/src/main/kotlin/dev/bogdanzurac/marp/feature/auth/ui/tracking.kt
@@ -0,0 +1,4 @@
+package dev.bogdanzurac.marp.feature.auth.ui
+
+internal const val LOGIN_SCREEN = "login"
+internal const val SIGNUP_SCREEN = "signup"
